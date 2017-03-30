de0de81a6ec2c9b0d60faa8bdf5ef999a39878fb
Pedro Faria <pfaf09@gmail.com>
2017-03-30 23:24:13 +0100

    Added dashboard of admin and owner

---
+++ pages/admin/home.php
@@ -0,0 +1,273 @@
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="row top_tiles">
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-caret-square-o-right"></i></div>
+                  <div class="count">179</div>
+                  <h3>New Sign ups</h3>
+                  <p>Lorem ipsum psdea itgum rixt.</p>
+                </div>
+              </div>
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-comments-o"></i></div>
+                  <div class="count">179</div>
+                  <h3>New Comments</h3>
+                  <p>Lorem ipsum psdea itgum rixt.</p>
+                </div>
+              </div>
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-check-square-o"></i></div>
+                  <div class="count">179</div>
+                  <h3>New Visitors</h3>
+                  <p>Lorem ipsum psdea itgum rixt.</p>
+                </div>
+              </div>
+            </div>
+          <!-- /top tiles -->
+
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="dashboard_graph">
+
+                <div class="row x_title">
+                  <div class="col-md-6">
+                    <h3>Web Activities <small>Registrations / Comments </small></h3>
+                  </div>
+                  <div class="col-md-6">
+                    <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                      <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                      <span>December 30, 2014 - January 28, 2015</span> <b class="caret"></b>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                  <div id="chart_plot_01" class="demo-placeholder"></div>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+
+          </div>
+          <br />
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="x_panel">
+                <div class="x_title">
+                  <h2>Top Users <small>Orders</small></h2>
+                  <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Settings 1</a>
+                        </li>
+                        <li><a href="#">Settings 2</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item One Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Three Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="x_panel">
+                <div class="x_title">
+                  <h2>Top Comments <small>Most replys</small></h2>
+                  <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Settings 1</a>
+                        </li>
+                        <li><a href="#">Settings 2</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item One Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Three Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="x_panel">
+                <div class="x_title">
+                  <h2>Top Books <small>Visits</small></h2>
+                  <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Settings 1</a>
+                        </li>
+                        <li><a href="#">Settings 2</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item One Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Two Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                  <article class="media event">
+                    <a class="pull-left border-aero profile_thumb">
+                      <i class="fa fa-user aero"></i>
+                    </a>
+                    <div class="media-body">
+                      <a class="title" href="#">Item Three Title</a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
