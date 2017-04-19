12762baefc8c097856f6825e58040d3366918180
agfac <andregfcorreia@gmail.com>
2017-04-19 19:12:47 +0100

    Quantity on cart already works

--- proto/pages/users/cart.php
+++ proto/pages/users/cart.php
@@ -1,5 +1,12 @@
 <?php
   include_once('../../config/init.php');
-  
+  include_once($BASE_DIR .'database/users.php');
+
+  $clientid = $_SESSION['userid'];
+
+  $publicationscart = getUserPublicationsCart(4);
+
+  $smarty->assign('publicationscart', $publicationscart);
+  $smarty->assign('qtOptions', array_combine(range(1,10),range(1,10)));
   $smarty->display('users/cart.tpl');
 ?>
