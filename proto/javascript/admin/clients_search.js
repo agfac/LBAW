d08863ae0d43d5b5239ec70cea7f2e409073ebb3
Fabiola Figueira da Silva <ffabiolasilva@gmail.com>
2017-05-23 13:14:49 +0100

    Sort and invert sort on cliente search invent after search, working

--- proto/javascript/admin/clients_search.js
+++ proto/javascript/admin/clients_search.js
@@ -1,17 +1,21 @@
 flag = true;
+sortByIdFlag = true;
+sortByNameFlag = false;
+sortByStatusFlag = false;
+
 $(document).ready(function() {
 
-    $('#search').on('click', function(){
+$(document).on('click', '#search', function () {
+    //$('#search').on('click', function(){
     	if (flag){
-            flag = false;
-	    	var nome_cliente = $('#nome_cliente').val();
+          flag = false;
+	    	  var nome_cliente = $('#nome_cliente').val();
 	        var email_cliente = $('#email_cliente').val();
 	        var estado_cliente = $('#estado_cliente').val();
 
 	        $('.clients_content').empty();
 
 	        $.getJSON("../../api/admin/clients_search.php", {nome_cliente: nome_cliente, email_cliente: email_cliente, estado_cliente: estado_cliente}, function(data){
-console.log(data);
 
 		        if(data.length === 0 || data == "NULL"){
 		            $('.clients_content').append('<p>Sem clientes com os dados de entrada</p>');
@@ -54,123 +58,75 @@ console.log(data);
         $('#estado_cliente').val("Escolha uma opção");
     });
 
-	$('#id').on('click', function(){
-		console.log("Ordena pelo id");
-		    sortTable();
-		function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("myTable");
-  switching = true;
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
-      //check if the two rows should switch place:
-      if (x.innerHTML > y.innerHTML) {
-        //if so, mark as a switch and break the loop:
-        shouldSwitch= true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
 
+  $(document).on('click', '#id', function () {
+		console.log("Ordena pelo id, flag = " + sortByIdFlag);
+		sortTable(0, "INTEGER", sortByIdFlag); 
+    sortByIdFlag = !sortByIdFlag;
 	});
 
-	$('#nome').on('click', function(){
-		console.log("Ordena pelo nome");
 
-				    sortTable();
-		function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("myTable");
-  switching = true;
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[1];
-      y = rows[i + 1].getElementsByTagName("TD")[1];
-      //check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //if so, mark as a switch and break the loop:
-        shouldSwitch= true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
+  $(document).on('click', '#nome', function () {
+		console.log("Ordena pelo nome, flag = " + sortByNameFlag);
+  	sortTable(1, "OUTRO", sortByNameFlag);
+    sortByNameFlag = !sortByNameFlag; 
 	});
 
-	$('#estado').on('click', function(){
-		console.log("Ordena pelo estado");
-				    sortTable();
-		function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("myTable");
-  switching = true;
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[2];
-      y = rows[i + 1].getElementsByTagName("TD")[2];
-      //check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //if so, mark as a switch and break the loop:
-        shouldSwitch= true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
+
+	$(document).on('click', '#estado', function () {
+		console.log("Ordena pelo estado, flag = " + sortByStatusFlag);
+    sortTable(2, "OUTRO", sortByStatusFlag); 
+    sortByStatusFlag = !sortByStatusFlag;
 	});
+
+
+
+ function sortTable(index, tipe, sortFlag) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    /*Make a loop that will continue until no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.getElementsByTagName("TR");
+      /*Loop through all table rows (except the first, which contains table headers):*/
+      for (i = 1; i < (rows.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare, one from current row and one from the next:*/
+       
+           if( tipe === "INTEGER") {
+              x = parseFloat( (rows[i].getElementsByTagName("TD")[index]).innerHTML );
+              y = parseFloat( (rows[i + 1].getElementsByTagName("TD")[index]).innerHTML );
+           }
+           else{
+             x = rows[i].getElementsByTagName("TD")[index].innerHTML.toLowerCase();
+             y = rows[i + 1].getElementsByTagName("TD")[index].innerHTML.toLowerCase();
+           }
+            
+          if (x > y && !sortFlag) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+          else if (x < y && sortFlag) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+      }
+
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+
+    } 
+
+  }
+
+
 
 });
