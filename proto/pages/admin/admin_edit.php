4bd1b8f30e6452762ece2b124dbec5a0ef2cd481
Pedro Faria <pfaf09@gmail.com>
2017-05-26 17:31:15 +0100

    Fixed errors verifications

--- proto/pages/admin/admin_edit.php
+++ proto/pages/admin/admin_edit.php
@@ -3,7 +3,7 @@ include_once('../../config/init.php');
 include_once($BASE_DIR .'database/admins.php');
 include_once('userInfo.php');
 
-if (!isset($_GET['username']) || empty($_GET['username']) || !checkIfAdminExists($_GET['username'])) {
+if (!($_GET['username']) || !checkIfAdminExists($_GET['username'])) {
 	error_log('if');
     $_SESSION['error_messages'][] = 'Erro com o username do Administrador';
     header("Location: $BASE_URL" . 'pages/admin/admins.php');
