897fad3a9ac8cfea04f4c872bb28c6398bd46101
Fabiola Figueira da Silva <ffabiolasilva@gmail.com>
2017-05-27 16:57:32 +0100

    Added queries to questions

--- proto/database/questions.php
+++ proto/database/questions.php
@@ -18,4 +18,62 @@ function updateQueryStatus($id){
     return $stmt->fetch();
 }
 
+function getQuestionsByUserNameAndStatus($nome_utilizador, $estadoPergunta){
+	global $conn;
+	$stmt = $conn->prepare("SELECT *
+							FROM perguntautilizador
+							WHERE LOWER(nome) like '%'||?||'%'
+							AND respondido = ?
+							ORDER BY perguntautilizadorid");
+
+	$stmt->execute(array(strtolower($nome_utilizador),$estadoPergunta));
+	return $stmt->fetchAll();
+}
+
+function getQuestionsByUserEmailAndStatus($email_utilizador, $estadoPergunta){
+	global $conn;
+	$stmt = $conn->prepare("SELECT *
+							FROM perguntautilizador
+							WHERE LOWER(email) = ?
+							AND respondido = ?
+							ORDER BY perguntautilizadorid");
+
+	$stmt->execute(array(strtolower($email_utilizador),$estadoPergunta));
+	return $stmt->fetchAll();
+}
+
+function getQuestionsByUserName($nome_utilizador){
+	global $conn;
+	$stmt = $conn->prepare("SELECT *
+							FROM perguntautilizador
+							WHERE LOWER(nome) like '%'||?||'%'
+							ORDER BY perguntautilizadorid");
+
+	$stmt->execute(array(strtolower($nome_utilizador)));
+	return $stmt->fetchAll();
+}
+
+function getQuestionsByUserEmail($email_utilizador){
+	global $conn;
+	$stmt = $conn->prepare("SELECT *
+							FROM perguntautilizador
+							WHERE LOWER(email) = ?
+							ORDER BY perguntautilizadorid");
+
+	$stmt->execute(array(strtolower($email_utilizador)));
+	return $stmt->fetchAll();
+}
+
+function getQuestionsByStatus($estadoPergunta){
+	global $conn;
+	$stmt = $conn->prepare("SELECT *
+							FROM perguntautilizador
+							WHERE respondido = ?
+							ORDER BY perguntautilizadorid");
+
+	$stmt->execute(array($estadoPergunta));
+	return $stmt->fetchAll();
+}
+
+
 ?>
