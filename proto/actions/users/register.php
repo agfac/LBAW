4164ba73e3b3d9259404927a87d071f513d4c922
agfac <andregfcorreia@gmail.com>
2017-05-27 20:32:24 +0100

    Modify exception verification to consider specific errors

--- proto/actions/users/register.php
+++ proto/actions/users/register.php
@@ -1,46 +1,52 @@
 <?php
-  include_once('../../config/init.php');
-  include_once($BASE_DIR .'database/users.php');  
+include_once('../../config/init.php');
+include_once($BASE_DIR .'database/users.php');  
 
-  if (!$_POST['nome'] || !$_POST['genero'] || !$_POST['diaNasc'] || !$_POST['mesNasc'] || !$_POST['anoNasc'] || !$_POST['morada'] || !$_POST['localidade'] || !$_POST['cod1'] || !$_POST['cod2'] || !$_POST['pais'] || !$_POST['email'] || !$_POST['username'] || !$_POST['password']) {
-    error_log('if');
-    $_SESSION['error_messages'][] = 'Todos os campos são de preenchimento obrigatório';
-    $_SESSION['form_values'] = $_POST;
-    header("Location: $BASE_URL" . 'pages/users/register.php');
-    exit;
-  }
+if (!$_POST['nome'] || !$_POST['genero'] || !$_POST['diaNasc'] || !$_POST['mesNasc'] || !$_POST['anoNasc'] || !$_POST['morada'] || !$_POST['localidade'] || !$_POST['cod1'] || !$_POST['cod2'] || !$_POST['pais'] || !$_POST['email'] || !$_POST['username'] || !$_POST['password']) {
+  error_log('if');
+  $_SESSION['error_messages'][] = 'Todos os campos são de preenchimento obrigatório';
+  $_SESSION['form_values'] = $_POST;
+  header("Location: $BASE_URL" . 'pages/users/register.php');
+  exit;
+}
 
-  $nome = strip_tags($_POST['nome']);
-  $genero = strip_tags($_POST['genero']);
-  $diaNasc = strip_tags($_POST['diaNasc']);
-  $mesNasc = strip_tags($_POST['mesNasc']);
-  $anoNasc = strip_tags($_POST['anoNasc']);
-  $morada = strip_tags($_POST['morada']);
-  $localidade = strip_tags($_POST['localidade']);
-  $cod1 = strip_tags($_POST['cod1']);
-  $cod2 = strip_tags($_POST['cod2']);
-  $pais = strip_tags($_POST['pais']);
-  $telefone = strip_tags($_POST['telefone']);
-  $email = strip_tags($_POST['email']);
-  $nif = strip_tags($_POST['nif']);
-  $username = strip_tags($_POST['username']);
-  $password = strip_tags($_POST['password']);
+$nome = strip_tags($_POST['nome']);
+$genero = strip_tags($_POST['genero']);
+$diaNasc = strip_tags($_POST['diaNasc']);
+$mesNasc = strip_tags($_POST['mesNasc']);
+$anoNasc = strip_tags($_POST['anoNasc']);
+$morada = strip_tags($_POST['morada']);
+$localidade = strip_tags($_POST['localidade']);
+$cod1 = strip_tags($_POST['cod1']);
+$cod2 = strip_tags($_POST['cod2']);
+$pais = strip_tags($_POST['pais']);
+$telefone = strip_tags($_POST['telefone']);
+$email = strip_tags($_POST['email']);
+$nif = strip_tags($_POST['nif']);
+$username = strip_tags($_POST['username']);
+$password = strip_tags($_POST['password']);
 
-  try {
+try {
 
-    createUser($nome, $genero, $diaNasc, $mesNasc, $anoNasc, $morada, $localidade, $cod1, $cod2, $pais, $telefone, $email, $nif, $username, $password);
-    
-  } catch (PDOException $e) {
+  createUser($nome, $genero, $diaNasc, $mesNasc, $anoNasc, $morada, $localidade, $cod1, $cod2, $pais, $telefone, $email, $nif, $username, $password);
   
-    if (strpos($e->getMessage(), 'cliente_email_key') !== false || strpos($e->getMessage(), 'cliente_nif_key') !== false || strpos($e->getMessage(), 'cliente_username_key') !== false) {
-      $_SESSION['error_messages'][] = 'Username escolhido já existe';
-    }
-    else $_SESSION['error_messages'][] = 'Erro ao criar utilizador';
-
-    $_SESSION['form_values'] = $_POST;
-    header("Location: $BASE_URL" . 'pages/users/register.php');
-    exit;
+} catch (PDOException $e) {
+  
+  if (strpos($e->getMessage(), 'cliente_email_key') !== false) {
+    $_SESSION['error_messages'][] = 'Já existe um utilizador com o email introduzido';
   }
-  $_SESSION['success_messages'][] = 'Utilizador registado com sucesso';  
-  header("Location: $BASE_URL");
+  else if(strpos($e->getMessage(), 'cliente_nif_key') !== false){
+    $_SESSION['error_messages'][] = 'Já existe um utilizador com o NIF introduzido';
+  }
+  else if(strpos($e->getMessage(), 'cliente_username_key') !== false){
+    $_SESSION['error_messages'][] = 'Já existe um utilizador com o username introduzido';
+  }
+  else $_SESSION['error_messages'][] = 'Erro ao criar utilizador';
+
+  $_SESSION['form_values'] = $_POST;
+  header("Location: $BASE_URL" . 'pages/users/register.php');
+  exit;
+}
+$_SESSION['success_messages'][] = 'Utilizador registado com sucesso';  
+header("Location: $BASE_URL");
 ?>
