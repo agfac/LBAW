70108b50efd02de0e6bc707d7b8405d40deea614
agfac <andregfcorreia@gmail.com>
2017-05-28 13:25:07 +0100

    Add ajax call to update items on basket when move it form wishlist to cart

--- proto/javascript/users/wishlist.js
+++ proto/javascript/users/wishlist.js
@@ -7,8 +7,12 @@ function updateWishListNumberItems() {
 	$('.btn').on('click', function (){
 
 		var tableBody = $(this).closest('tbody');
-		var itemSelected = this.closest('tr');
-		var idSelected = itemSelected.getAttribute('data-id');
+		var itemSelected = $(this).closest('tr');
+		var idSelected = itemSelected.attr('data-id');
+		var itemLinkSelected = itemSelected.find('a').attr('href');
+		var itemImageSelected = itemSelected.find('img').attr('src');
+		var itemTitle = itemSelected.find('h6 a').text();
+		var itemPrice = itemSelected.attr('data-price');
 
 		$.getJSON("../../api/users/move_item_wishlist_to_cart.php", {id: idSelected}, function(data) {
 
@@ -31,9 +35,11 @@ function updateWishListNumberItems() {
 				});
 
 				$.getJSON("../../api/users/get_cart_items.php", function(data) {
+					console.log(data);
 					if (data > 0) {
 						$('.fa-shopping-cart').parent().find('span').text("(" + data + ")");
 						$('.fa-shopping-basket').parent().find('span .text-primary').text("(" + data + ")");
+						$('.cart-items').find('.items').append('<li data-id="' + idSelected + '"><a href="' + itemLinkSelected + '" class="product-image"><img src="' + itemImageSelected + '" alt="Sample Product "></a><div class="product-details"><div class="close-icon"> <a href="javascript:void(0);"><i class="fa fa-close"></i></a></div><p class="product-name"> <a href="' + itemLinkSelected + '">' + itemTitle + '</a></p><strong data-type="quantidade">' + 1 + '</strong> x <span class="price text-primary">€' + itemPrice + '</span></div><!-- end product-details --></li><!-- end item -->');
 					}
 				});
 
