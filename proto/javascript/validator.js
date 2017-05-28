c807f77b93912fc6fa58513f0e63266489edfb4c
Fabiola Figueira da Silva <ffabiolasilva@gmail.com>
2017-05-28 03:29:43 +0100

    Title removed when no longer needed

--- proto/javascript/validator.js
+++ proto/javascript/validator.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	var passFlag = false;
 
 	$("#password").on('change input', function(){
 		var passwordLenght = ($(this).val()).length;
@@ -9,14 +8,13 @@ $(document).ready(function() {
 			div.addClass("has-success");
 			div.removeClass("has-error");
 			$("#submit").prop("disabled", false);
-			passFlag = true;
+			$("#submit").removeAttr( "title" );
 		}
 		else{
 			div.addClass("has-error");
 			$("#submit").prop("disabled", true);
 			$(this).prop("title", "A password tem que ter mais de 6 carateres");	
 			$("#submit").prop("title", "Verifique a password");
-			passFlag = false;
 		}
 		
 	});
